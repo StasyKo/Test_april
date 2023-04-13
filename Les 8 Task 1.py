871a857989e66a24e7d2f5b6c1dcff6eb237988c
StasyKo <128061200+StasyKo@users.noreply.github.com>
2023-04-13 09:09:23 +0300

    Add files via upload

--- Les 8 Task 1.py
+++ Les 8 Task 1.py
@@ -1,4 +1,4 @@
-#1. Реализовать класс «Дата», функция-конструктор которого должна принимать
+# 1. Реализовать класс «Дата», функция-конструктор которого должна принимать
 # дату в виде строки формата «день-месяц-год». В рамках класса реализовать
 # два метода. Первый, с декоратором @classmethod. Он должен извлекать число,
 # месяц, год и преобразовывать их тип к типу «Число». Второй, с декоратором
@@ -13,15 +13,12 @@ class Data:
     @classmethod
     def extract(cls, d_m_y):
         my_date = []
-
         for i in d_m_y.split():
-            if i != '-': my_date.append(i)
-
+            if i != "-": my_date.append(i)
         return int(my_date[0]), int(my_date[1]), int(my_date[2])
 
     @staticmethod
     def valid(day, month, year):
-
         if 1 <= day <= 31:
             if 1 <= month <= 12:
                 if 2023 >= year >= 0:
@@ -41,7 +38,7 @@ today = Data('12 - 4 - 2023')
 
 print(today)
 print(Data.valid(21, 4, 2019))
-print(today.valid(6, 25, 2018))
+print(today.valid(6, 25, 2016))
 print(Data.extract('12 - 12 - 2012'))
 print(today.extract('11 - 11 - 2011'))
 print(Data.valid(12, 12, 2019))
